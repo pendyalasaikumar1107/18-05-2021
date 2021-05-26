766b468a5580afc84db64f69e19e24cc5f13b9ab
Rohan657 <83385511+Rohan657@users.noreply.github.com>
2021-05-26 15:08:09 +0530

    Delete profileEvaluation/src/main/java/com/tcs/evaluation/profileEvaluation directory

--- profileEvaluation/src/main/java/com/tcs/evaluation/profileEvaluation/Repository/EvaluatorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tcs.evaluation.profileEvaluation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tcs.evaluation.profileEvaluation.entity.Evaluator;
-
-public interface EvaluatorRepo extends JpaRepository<Evaluator, Integer>{
-
-}
--- profileEvaluation/src/main/java/com/tcs/evaluation/profileEvaluation/Repository/StatusRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tcs.evaluation.profileEvaluation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tcs.evaluation.profileEvaluation.entity.Profilestatus;
-
-public interface StatusRepo extends JpaRepository<Profilestatus, Integer> {
-
-}
